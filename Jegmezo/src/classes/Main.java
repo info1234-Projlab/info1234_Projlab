e4a1607af8f7890571af9bdc1f2b642d8fc89b6f
szukiadam <szukiadam@gmail.com>
2020-03-28 18:31:14 +0100

    UseAbility() Eskimo

--- Jegmezo/src/classes/Main.java
+++ Jegmezo/src/classes/Main.java
@@ -1,16 +1,31 @@
 package classes;
+import java.util.Scanner;
 
-public class Main {
+public class Main{
+	public static void main(String args[]) {
+		UseAbilityEskimo();
+	}
 
-}
 
-public void UseAbilityEskimo() {
+public static void UseAbilityEskimo() {
 	Field f = new Field(3, 3);
 	Eskimo e = new Eskimo(f);
+	System.out.printf("Add meg, hány akciója van még hátra a játékosnak (0-4), van-e iglu már a mezőn (0-1). \n");
+	Scanner in = new Scanner(System.in);
+	int actions = in.nextInt();
+	int b = in.nextInt();
+	boolean bool;
+	if(b ==0) {bool = false;}	
+		else	{bool=true;}
+	e.SetNumOfAction(actions);
+	f.SetHasIglu(bool);
+	if(e.GetNumOfAction() == 0 ) { System.out.printf("Nem tud akciót végrehajtani! \n");}
 	if(e.GetNumOfAction() > 0 ) {
 		if(f.CanBuildIglu()) {
 		f.SetHasIglu(true);
 		e.DecreaseAction();
 		}	
 	}
+}
+
 }
