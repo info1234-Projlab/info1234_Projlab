dc8cb0562c57b177529a7e5bcfa221110177b584
szukiadam <szukiadam@gmail.com>
2020-03-29 15:09:14 +0200

    additem in main

--- Jegmezo/src/classes/Main.java
+++ Jegmezo/src/classes/Main.java
@@ -43,12 +43,34 @@ public static void UseAbilityExplorer() {
 }
 
 public static void AddItemToField() {
-	System.out.printf("Add meg, hany reteg hó legyen a mezõn\n");
+	System.out.printf("Add meg, hany reteg hï¿½ legyen a mezï¿½n\n");
 	Scanner in = new Scanner(System.in);
 	int layer=in.nextInt();
 	Field f=new Field(1,layer);
 	Explorer e=new Explorer(f); 
-	System.out.printf("A tárgy búvárruha legyen (1) vagy egyéb (2)?\n");
+	System.out.printf("A tï¿½rgy bï¿½vï¿½rruha legyen (1) vagy egyï¿½b (2)?\n");
+	Scanner in2 = new Scanner(System.in);
+	int itemkind=in2.nextInt();
+	if (itemkind==2) {
+		Food todropitem=new Food(0,true);
+		f.AddItem(todropitem,1);
+		todropitem.Drop(e,1);
+	}
+	if (itemkind==1) {
+		DivingSuit todropitem=new DivingSuit(0,true,true);
+		f.AddItem(todropitem,1);
+		todropitem.Drop(e,1);
+		}
+	
+}
+
+public static void AddItemToField() {
+	System.out.printf("Add meg, hany reteg hï¿½ legyen a mezï¿½n\n");
+	Scanner in = new Scanner(System.in);
+	int layer=in.nextInt();
+	Field f=new Field(1,layer);
+	Explorer e=new Explorer(f); 
+	System.out.printf("A tï¿½rgy bï¿½vï¿½rruha legyen (1) vagy egyï¿½b (2)?\n");
 	Scanner in2 = new Scanner(System.in);
 	int itemkind=in2.nextInt();
 	if (itemkind==2) {
