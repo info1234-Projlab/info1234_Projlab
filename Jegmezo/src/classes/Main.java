dae9e91c3861c30f61ce3038055104ad7476c243
szukiadam <szukiadam@gmail.com>
2020-04-25 23:27:53 +0200

    matrix corrected

--- Jegmezo/src/classes/Main.java
+++ Jegmezo/src/classes/Main.java
@@ -338,9 +338,11 @@ public class Main{
 				ArrayList<Field> f=new ArrayList<Field>(fields.values());
 				for(int i1 =1;i1<numberOfFields+1;i1++) {
 					for(int j=1;j<numberOfFields+1;j++) {
-						if(matrix.charAt(i1*j-1)=='1') {
+						if(matrix.charAt((i1-1)*numberOfFields+j-1)=='1') {
 							f.get(i1-1).AddNeighbour(f.get(j-1));
-							System.out.println(i1 + " es " + j + "szomszedos");
+							int a = i1-1;
+							int b = j-1;
+							System.out.println( a+ " es " + b + "szomszedos");
 						}
 							
 					}
