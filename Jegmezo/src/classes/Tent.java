71bdb79224bc72b09bc6e199ea921239a5ca7799
szukiadam <szukiadam@gmail.com>
2020-04-26 22:17:32 +0200

    testek O->o

--- Jegmezo/src/classes/Tent.java
+++ Jegmezo/src/classes/Tent.java
@@ -1,11 +1,24 @@
 package classes;
-
+/**
+ * 
+ * A sator targy birtoklasanak es elvesztesenek kovetkezmenyeinek kezelese. Fel lehet
+venni a mezorol ha kiastuk a ho alol es le is lehet tenni egy mezore ahol
+menedekkent hasznalhato. Ved a hovihar ellen azonban a jegesmedve tamadas
+ellen nem hasznal. Ebben az esetben a sator eszkozkent viselkedik. Fel lehet venni,
+el lehet dobni es hasznalni lehet. 
+ * 
+ */
 public class Tent extends Item {
 
 	public Tent(int layer, boolean visible) {
 		super(layer, visible);
 	}
 	
+	/**
+	 * 
+	 * @param p
+	 * @return
+	 */
 	public boolean PutOnField(Player p) {
 		if(p.GetNumOfAction()<=0)
 			return false;
