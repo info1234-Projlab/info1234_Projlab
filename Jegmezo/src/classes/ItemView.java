ae39d0b10e815d50030565569b1e929cc3245b6f
GerryCarry <39331952+GerryCarry@users.noreply.github.com>
2020-05-17 08:56:18 +0200

    Update ItemView.java

--- Jegmezo/src/classes/ItemView.java
+++ Jegmezo/src/classes/ItemView.java
@@ -10,6 +10,8 @@ import javax.imageio.ImageIO;
 
 public class ItemView {
 	protected Image img;
+	private Point coordinates;
+	private Point center;
 	
 	public ItemView(String path){
 		try {
@@ -17,9 +19,29 @@ public class ItemView {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		coordinates = new Point(0,0);
+		center = new Point(25,25);
 	}
 	
-	public void Draw(Point coordinates, Graphics g) {
+	public void Draw( Graphics g) {
 		g.drawImage(img, coordinates.x, coordinates.y, null);
+	}
+	
+	public void setPoint(Point p){
+		coordinates = p;
+		center.x = p.x+25;
+		center.y = p.y+25;
+	}
+	
+	public boolean CheckClicked(Point p) {
+		Point center = new Point(coordinates.x + 25, coordinates.y + 25);
+		double a = (p.x-center.x) * (p.x-center.x);
+        double b = (p.y-center.y) * (p.y-center.y);
+        double cp = 22 * 22;
+        if(a + b < cp ){
+        	return true;
+        }
+        return false;
 	}
 }
